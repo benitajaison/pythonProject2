5aad36f44700bc5f2acecc09e7ef512c848c2e29
Benita <benitajaison@gmail.com>
2023-06-11 18:26:05 +0530

    try

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
-from flask import Flask , redirect,render_template,request
+from flask import Flask,render_template , request
 import numpy as np
 import joblib
 
 app=Flask(__name__)
 
-@app.route("/")
+
+@app.route('/')
 def home():
     return render_template('index.html')
-@app.route("/submit",methods=['POST','GET'])
+@app.route("/submit",methods=['POST','GET'])   
 def predict():
     if request.method=='POST':
         eop=float(request.form['EOP'])
-        reg = joblib.load('regression_model.joblib')
-        data=np.array([eop]).reshape(1,-1)
+        reg=joblib.load('regression_model.joblib')
+        data=np.array([eop]).reshap(1,-1)
         result=reg.predict(data)
-        result=str(result)
+        result= str (result)
     return result
+def ab():
+    return render_template('aboutus.html')
 
 if __name__== '__main__':
     app.run(debug=True)
